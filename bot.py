af9d78ce306841f8a88dca35623de073172685c2
PeterMagdyIskander <PeterMagdyIskander@gmail.com>
2021-05-03 21:21:44 +0200

    new splendid commit WineTime

--- bot.py
+++ bot.py
@@ -18,7 +18,8 @@ choices=[
     'who is pokimane? 🙄🤭🤔🤫😰 in math: my solution ➗😊 in history: my queen 👑😣 in art: my canvas 🎨🥳 in science: my oxygen 💨😝 in geography: my world 🌎🤯',
     'TRUE KEKW TRUE KEKW TRUE KEKW TRUE KEKW TRUE KEKW TRUE KEKW TRUE KEKW TRUE KEKW TRUE KEKW ',
     '(◕‿◕✿) Dear Weebs in the chat AYAYA, you are sugoi AYAYA. Whatever is going on in your kokoro right now AYAYA, please know that you are kawaii and your story is not a filler AYAYA. You are loved (◕‿◕✿)',
-    'I feel so bad for people who unironically like anime. Like, they didn\'t choose to be born with an IQ of 20 in a home with unloving parents. Imagine all the bullying they must experience at home, at school, and at the special services daycare. Let\'s stop bullying them, guys. They may be too retarded to understand that\'s what we\'re doing, but that doesn\'t mean we should pick on them. Thank you'   
+    'I feel so bad for people who unironically like anime. Like, they didn\'t choose to be born with an IQ of 20 in a home with unloving parents. Imagine all the bullying they must experience at home, at school, and at the special services daycare. Let\'s stop bullying them, guys. They may be too retarded to understand that\'s what we\'re doing, but that doesn\'t mean we should pick on them. Thank you',
+    ' gachiBASS gachiBASS gachiBASS gachiBASS gachiBASS gachiBASS gachiBASS gachiBASS gachiBASS gachiBASS gachiBASS gachiBASS gachiBASS gachiBASS gachiBASS gachiBASS gachiBASS gachiBASS gachiBASS'   
 ]
 
 
